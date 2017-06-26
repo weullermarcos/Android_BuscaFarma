6cef716d5e26b4ced081b2dbae4dd935a188a544
Weuller Marcos <weullermarcos@hotmail.com>
2017-06-26 19:16:09 -0300

    criando id para objetos da tela

--- BuscaFarma/app/src/main/java/com/weullermarcos/buscafarma/MainActivity.java
+++ BuscaFarma/app/src/main/java/com/weullermarcos/buscafarma/MainActivity.java
@@ -3,14 +3,21 @@ package com.weullermarcos.buscafarma;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtRemedy;
+    ListView lstPharmacy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("MENSAGEM: ","PASSOU PELA MAIN");
+        edtRemedy = (EditText) findViewById(R.id.edtRemedy);
+        lstPharmacy = (ListView) findViewById(R.id.lstPharmacy);
+
     }
 }
